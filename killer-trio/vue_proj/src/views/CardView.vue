54c0d75f86eb552d34bd968e53a4a4bee856fcc4
Julia Koblmiller <juliakoblmiller@Julias-MacBook-Pro.local>
2022-04-02 13:51:57 +0200

    bieten login abfrage

--- killer-trio/vue_proj/src/views/CardView.vue
+++ killer-trio/vue_proj/src/views/CardView.vue
@@ -4,38 +4,32 @@
       <div class="col-1"></div>
       <div class="col-4">
         <div>
-          <img
-            class="img-fluid"
-            :src="auction.image_url"
-            :alt="auction.product"
-          />
+          <img class="img-fluid" :src="auction.image_url" :alt="auction.product" />
           <br />
           <h6 class="badge rounded-pill bg-secondary">User: {{ user }}</h6>
         </div>
       </div>
-      <div
-        class="col-6 d-flex justify-content-center flex-column text-start p-4"
-      >
+      <div class="col-6 d-flex justify-content-center flex-column text-start p-4">
         <h1 class="fw-bold fs-1">{{ auction.product.toUpperCase() }}</h1>
-        <p class="fs-3">
-          Mindestangebot: €{{ this.auctionMinPrice  + ",00"}}
-        </p>
-
-        <button
-          data-bs-toggle="modal"
-          class="btn btn-primary p-2 shadow fs-4 fw-bold"
-          data-bs-target="#bietenButton"
-        >
-          {{ "AUF " + auction.product.toUpperCase() + " BIETEN" }}
-        </button>
-        <Modal
-          :title="this.auction.product"
-          :text="'offerexampletest'"
-          :btnText="'Bid Now'"
-          :minPrice="this.auctionMinPrice"
-          id="bietenButton"
-        />
+        <p class="fs-3">Mindestangebot: €{{ this.auctionMinPrice + ",00"}}</p>
+        <div v-if="this.$store.state.loggedIn">
+          <button
+            data-bs-toggle="modal"
+            class="btn btn-primary p-2 shadow fs-4 fw-bold"
+            data-bs-target="#bietenButton"
+          >{{ "AUF " + auction.product.toUpperCase() + " BIETEN" }}</button>
+          <Modal
+            :title="this.auction.product"
+            :text="'offerexampletest'"
+            :btnText="'Bid Now'"
+            :minPrice="this.auctionMinPrice"
+            id="bietenButton"
+          />
+        </div>
+      <div v-else>
+        <router-link to="/access" class="nav-link">Login zum Bieten</router-link>
       </div>
+    </div>
     </div>
     <div v-bind:key="offer.id" v-for="offer in this.offers">
       <Offer class="m-2" :price="offer.price" :amount="offer.amount" />
@@ -44,82 +38,82 @@
 </template>
 
 <script>
-import axios from 'axios';
-import Modal from '../components/Modal.vue';
-import AuctionModal from '../components/AuctionModal.vue';
-import Picture from '../components/atoms/Picture.vue';
-import OfferCard from '../components/molecules/OfferCard.vue';
-import Offer from '../components/molecules/Offer.vue';
+import axios from "axios";
+import Modal from "../components/Modal.vue";
+import AuctionModal from "../components/AuctionModal.vue";
+import Picture from "../components/atoms/Picture.vue";
+import OfferCard from "../components/molecules/OfferCard.vue";
+import Offer from "../components/molecules/Offer.vue";
 
 export default {
-  name: 'CardView',
+  name: "CardView",
   components: {
     Modal,
     AuctionModal,
     Picture,
     OfferCard,
-    Offer,
+    Offer
   },
   props: {
     product_id: {
-      type: String,
+      type: String
     },
     product: {
       type: String,
-      default: 'Getränkename',
+      default: "Getränkename"
     },
     description: {
       type: String,
-      default: 'this is the product description',
+      default: "this is the product description"
     },
     product_category: {
       type: String,
-      default: 'Bier',
+      default: "Bier"
     },
     pic_url: {
       type: String,
-      default: 'logo.png',
+      default: "logo.png"
     },
     product_h_bid: {
       type: Number,
-      default: 0.0,
+      default: 0.0
     },
     user: {
       type: String,
-      default: 'BeverageBidder56',
-    },
+      default: "BeverageBidder56"
+    }
   },
   data() {
     return {
       auction: {
-        product: 'Test',
-        offers: null,
+        product: "Test",
+        offers: null
       },
-      auctionMinPrice: 1,
+      auctionMinPrice: 1
     };
   },
   created() {
     // link not working, todo: load specific id again
-    this.$store.dispatch('getOffers');
+    this.$store.dispatch("getOffers");
     this.offers = null;
-    console.log('Card View erstellt');
+    console.log("Card View erstellt");
     const fullPath = this.$router.currentRoute.path; // "/items/23";
-    const splittedFullPath = fullPath.split('/items/');
+    const splittedFullPath = fullPath.split("/items/");
     const idToLoad = splittedFullPath[1];
 
-    this.$store.state.auctions.forEach((auction) => {
+    this.$store.state.auctions.forEach(auction => {
       if (auction.id == idToLoad) {
         this.auction = auction;
       }
     });
 
-  let minOfferPrice = this.auction.minPrice ?? 0;
+    let minOfferPrice = this.auction.minPrice ?? 0;
 
-    this.$store.state.offers.forEach((offer) => {
+    this.$store.state.offers.forEach(offer => {
       console.log("Hi ich bin ein loop");
       console.log(offer.auctionId);
       if (offer.auctionId == idToLoad) {
-        console.log("Match: " +idToLoad);
+        console.log("Match: " + idToLoad);
         if (offer.price > minOfferPrice) {
           // do something
           minOfferPrice = offer.price;
@@ -130,9 +124,9 @@ export default {
     this.auctionMinPrice = minOfferPrice;
     var _this = this.$store;
     // load offers from store
-    var refresher = setInterval(function () {
-      _this.dispatch('refreshAuctions', idToLoad);
+    var refresher = setInterval(function() {
+      _this.dispatch("refreshAuctions", idToLoad);
     }, 10000);
-  },
+  }
 };
 </script>
